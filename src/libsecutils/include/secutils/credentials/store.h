af895db984953ebf452552103336895fc00a9dba
Dr. David von Oheimb <dev@ddvo.net>
2025-10-15 19:16:19 +0200

    STORE_set1_host{,_ip}(),STORE_get0_host(): clean up for OpenSSL 3.0+: remove ex_data->host and update comments

--- src/libsecutils/include/secutils/credentials/store.h
+++ src/libsecutils/include/secutils/credentials/store.h
@@ -252,7 +252,8 @@ bool STORE_EX_check_index(void);
  * @param store the affected certificate store
  * @param host the host name to set, or null to clear it
  * @return true on success, false on failure
- * @note since OpenSSL 3.0, this is no more needed due to X509_VERIFY_PARAM_get0_host() being available
+ * @note since OpenSSL 3.0, due to X509_VERIFY_PARAM_get0_host() being available,
+ *       this function is no more needed and just returns true
  */
 bool STORE_set1_host(X509_STORE* store, OPTIONAL const char* host);
 
